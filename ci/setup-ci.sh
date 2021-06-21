936002837bdfacb564be916ef1b4a65573ecd02f
Ian McGaunn <ianmcgaunn@gmail.com>
2021-06-21 16:04:26 -0400

    extract doctl as sudo

--- ci/setup-ci.sh
+++ ci/setup-ci.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 ARTIFACT_NAME="doctl-1.60.0-linux-amd64.tar.gz"
 wget https://github.com/digitalocean/doctl/releases/download/v1.60.0/${ARTIFACT_NAME}
-tar xzf ~/${ARTIFACT_NAME} -C /usr/local/bin/
+sudo tar xzf ~/${ARTIFACT_NAME} -C /usr/local/bin/
